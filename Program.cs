70b5d5415c227c7b1da3ea87dd6932ce482bcac9
Adrian F. Daquiz <kazukill.yomiro@gmail.com>
2023-03-27 23:09:52 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KZK03272023_PracticeGithubOnVS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
