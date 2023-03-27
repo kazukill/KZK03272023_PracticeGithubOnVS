6029a6c414614d9112597af68210314c05853af3
Adrian F. Daquiz <kazukill.yomiro@gmail.com>
2023-03-27 23:25:21 +0800

    added Calculator Class with only Add for now

---
+++ Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KZK03272023_PracticeGithubOnVS
+{
+    class Calculator
+    {
+        public static int Addition(int firstNum, int secondNum)
+        {
+            int x = firstNum + secondNum;
+                return x;
+        }
+    }
+}
